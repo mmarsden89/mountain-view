96d9e7ec564f93232689a8039e4dbf9153dd46da
mmarsden89 <mattmarsdendev@gmail.com>
2021-02-03 20:13:53 -0500

    mountain stylings

--- src/Components/Mountains/Mountains.js
+++ src/Components/Mountains/Mountains.js
@@ -1,10 +1,12 @@
 import "./mountains.scss";
 import SingleMountain from "./SingleMountain";
+import WideMountain from "./WideMountain";
 import Foreground from "./Foreground";
 
 const Mountains = () => {
   return (
     <div className="mountain-container">
+      <WideMountain />
       <SingleMountain />
       <Foreground />
       <div className="foreground-left"></div>

ed9d674932298d40f2e6e0d761751d97f6bee147
mmarsden89 <mattmarsdendev@gmail.com>
2021-02-03 20:13:19 -0500

    add widemountain component

---
+++ src/Components/Mountains/WideMountain.js
@@ -0,0 +1,14 @@
+const WideMountain = () => {
+  return (
+    <div className="wide-mountain">
+      <div class="wide-cap-1"></div>
+      <div class="wide-cap-2"></div>
+      <div class="wide-cap-3"></div>
+      <div class="wide-cap-4"></div>
+      <div class="wide-cap-5"></div>
+      <div class="wide-cap-6"></div>
+    </div>
+  );
+};
+
+export default WideMountain;

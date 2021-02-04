4a3b16b0257cbdac0a43ef109779484658e49aa7
mmarsden89 <mattmarsdendev@gmail.com>
2021-02-03 19:43:26 -0500

    add mountains component files

--- src/Components/Mountains/Mountains.js
+++ src/Components/Mountains/Mountains.js
@@ -1,8 +1,16 @@
-import react from "react";
 import "./mountains.scss";
+import SingleMountain from "./SingleMountain";
+import Foreground from "./Foreground";
 
 const Mountains = () => {
-  return <div className="mountain-container"></div>;
+  return (
+    <div className="mountain-container">
+      <SingleMountain />
+      <Foreground />
+      <div className="foreground-left"></div>
+      <div className="foreground-right"></div>
+    </div>
+  );
 };
 
 export default Mountains;

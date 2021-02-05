820951a048bb77933d54fb9d53b6fe26ff980c34
mmarsden89 <mattmarsdendev@gmail.com>
2021-02-05 15:17:09 -0500

    work on moon path

--- src/Components/CelestialContainer/Moon.js
+++ src/Components/CelestialContainer/Moon.js
@@ -1,7 +1,12 @@
 const Moon = () => {
   return (
     <div className="moon-container">
-      <div className="moon"></div>
+      <div className="moon">
+        <div className="crater"></div>
+        <div className="crater2"></div>
+        <div className="crater3"></div>
+        <div className="crater4"></div>
+      </div>
     </div>
   );
 };

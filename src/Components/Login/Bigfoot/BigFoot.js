9a99c1eb3fd6416505dfa84bb9cb20c00317af99
mmarsden89 <mattmarsdendev@gmail.com>
2021-02-08 16:09:32 -0500

    set up big foot container

---
+++ src/Components/Login/Bigfoot/BigFoot.js
@@ -0,0 +1,11 @@
+import "./bigfoot.scss";
+
+const BigFoot = () => {
+  return (
+    <div className="bigfoot-container">
+      <div className="head"></div>
+    </div>
+  );
+};
+
+export default BigFoot;

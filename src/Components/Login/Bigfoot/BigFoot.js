e26e5f3fc4d12a8afaa10eaedfe609046f427c61
mmarsden89 <mattmarsdendev@gmail.com>
2021-02-08 17:15:05 -0500

    start signin animation  functionality

--- src/Components/Login/Bigfoot/BigFoot.js
+++ src/Components/Login/Bigfoot/BigFoot.js
@@ -3,11 +3,21 @@ import "./bigfoot.scss";
 const Bigfoot = () => {
   return (
     <div className="bigfoot-container">
-      <div className="head"></div>
-      <div className="eye-container">
-        <div className="eye1"></div>
-        <div className="eye2"></div>
+      <div className="body-container">
+        <div className="ear1"></div>
+        <div className="ear2"></div>
+        <div className="head"></div>
+        <div className="eye-container">
+          <div className="eye1"></div>
+          <div className="eye2"></div>
+        </div>
+        <div className="mouth-container">
+          <div className="nose"></div>
+          <div className="mouth-line"></div>
+          <div className="mouth"></div>
+        </div>
       </div>
+      <button className="signin-bar">sign in</button>
     </div>
   );
 };
